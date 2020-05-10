6545e691f524b17fb052704206a05fd5a53a9451
Volfee <przemyslaw.wilk7@gmail.com>
2020-05-10 12:25:00 +0100

    New cards are saved to storage.

--- flashcards.py
+++ flashcards.py
@@ -1,9 +1,10 @@
-import json
+from storage_manager import StorageManager
 
 class Flashcard:
-	next_id = 0
+	last_id = 0
+	sm = StorageManager()
 
-	def __init__(self, front, back='', deck='default', id=None):
+	def __init__(self, front, back, deck='default', id=None):
 		if id:
 			self.id = id
 		else:
@@ -11,28 +12,28 @@ class Flashcard:
 		self.front = front
 		self.back = back
 
-	def to_json(self):
+	def _create_id(self):
+		if not Flashcard.last_id:
+			Flashcard.last_id = self.sm.get_last_id()
+		Flashcard.last_id += 1
+		return Flashcard.last_id
+
+	def save(self):
+		self.sm.save_card(self)
+
+	def to_dict(self):
 		"""Returns json representation of the card."""
-		flashcard = {
+		dictionary = {
 			'id': self.id,
 			'front': self.front,
 			'back': self.back
 		}
-		json_card = json.dumps(flashcard)
-		return json_card
-
-	def _check_next_id(self):
-		Flashcard.next_id = StorageManager.get_last_id() + 1
-
-	def _create_id(self):
-		if Flashcard.next_id == 0:
-			self._check_next_id()
-		return Flashcard.next_id
-
+		return dictionary
 
 
 class Base(Flashcard):
 	pass
+
 
 class Cloze(Flashcard):
 	pass
--- storage_manager.py
+++ storage_manager.py
@@ -1,17 +1,44 @@
+import json
+
 class StorageManager:
 	"""Handles saving and loading flashcards from drive."""
-	storage = "storage/cards.json"
+	storage = 'storage/cards.json'
+	metadata = 'storage/metadata'
 
-	def __init__(self):
-		pass
+	def __init__(self, storage=None):
+		if storage:
+			self.storage = storage
 
 	def save_card(self, flashcard):
-		json_card = flashcard.to_json()
-		with open(StorageManager.storage, 'a') as f:
-			f.write(json_card)
+		with open(self.storage, 'a') as file:
+			json.dump(flashcard.to_dict(), file)
+		self.store_last_id(flashcard.id)
 
-	def load_card(self, id='next'):
+	def load_all_cards(n=0):
+		with open(self.storage) as cards_file:
+			cards = json.load(cards_file)
+		return cards
+
+	def get_last_id(self):
+		try:
+			with open(self.metadata) as meta:
+				metadict = json.load(meta)
+			return metadict['last_id']
+
+		except json.decoder.JSONDecodeError as err:
+			print("No cards in database yet. Returning id 1.")
+			return 0
+
+	def store_last_id(self, last_id):
+		to_save = {'last_id': last_id}
+		with open(self.metadata, 'w') as meta:
+			json.dump(to_save, meta)
+
+	def clear_storage(self):
 		pass
 
-	def load_all_cards(self):
+	def remove_card(self):
+		"""Removing card will result in ids mismatching number of rows.
+		Changes to get_last_id will have to be made.
+		"""
 		pass
