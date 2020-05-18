4252de02c1132fdad79800d1bd2e6ecda0df6eaf
Volfee <przemyslaw.wilk7@gmail.com>
2020-05-18 20:19:12 +0100

    Addng review page to main view class

--- interface/main_view.py
+++ interface/main_view.py
@@ -1,17 +1,21 @@
 import tkinter as tk
 from main_page import MainPage
 from add_flashcard import AddFlashcard
+from review_page import ReviewPage
 
 class MainView(tk.Frame):
 	def __init__(self, *args, **kwargs):
 		tk.Frame.__init__(self, *args, **kwargs)
 		self.add_flashcard_page = AddFlashcard(master=self)
 		self.main_page = MainPage(master=self)
+		self.review_page = ReviewPage(master=self)
+
 		
 		container = tk.Frame(self)
 		container.pack(side="top", fill="both", expand=True)
 		self.main_page.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 		self.add_flashcard_page.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+		self.review_page.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
 		self.main_page.show()
 
